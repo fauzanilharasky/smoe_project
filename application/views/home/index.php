d0d7c827fc71339d0017245eff3b14df0d83789e
Fauzan Ilharasky <fzn.ilhrsky@gmail.com>
2025-08-31 15:34:39 +0700

    three update main page

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,31 +1,31 @@
 <section class="container-lg mt-5 mb-5">
   <div class="card shadow-sm">
     <div class="card-header bg-primary text-white shadow-lg">
-      <h4 class="mb-0 text-center">Daftar Data Master</h4>
+      <h4 class="mb-0 text-center">Master Data List</h4>
     </div>
     <div class="card-body">
       <div class="table-responsive">
         <table class="table table-bordered table-striped mb-0">
           <thead class="table-secondary">
-            <tr class="text-white">
-              <th class="text-dark">No</th>
-              <th class="text-dark">Nama</th>
+            <tr>
+              <th class="text-dark">No.</th>
+              <th class="text-dark">Name</th>
               <th class="text-dark">Email</th>
-              <th class="text-dark">Role</th>
+              <th class="text-dark">No. Badge</th>
+              <th class="text-dark">Address Company</th>
+			  <th class="text-dark">Depatement Name</th>
+			  <th class="text-dark">project name</th>
+			  <th class="text-dark">Description</th>
+              <th class="text-dark">Action</th>
             </tr>
           </thead>
           <tbody>
+
+            <!-- Jika data kosong -->
             <tr>
-              <td>1</td>
-              <td>Fauzan</td>
-              <td>fauzan@mail.com</td>
-              <td>Admin</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Ilharasky</td>
-              <td>ilharasky@mail.com</td>
-              <td>User</td>
+              <td colspan="6" class="text-center text-muted py-3">
+                No data available yet.
+              </td>
             </tr>
           </tbody>
         </table>
@@ -33,5 +33,3 @@
     </div>
   </div>
 </section>
-
-
--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -8,12 +8,11 @@
     </div>
 
     <ul class="sidebar-menu">
-      <li class="menu-header">داشبورد</li>
+      <li class="menu-header">Sidebar Menu</li>
       <li class="dropdown active">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i> <span>menu</span></a>
+        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i> <span>Home</span></a>
         <ul class="dropdown-menu">
-          <li class="active"><a class="nav-link" href="<?= base_url('dashboard'); ?>">Home</a></li>
-          <li><a class="nav-link" href="<?= base_url('sales'); ?>">Sales</a></li>
+          <li><a class="nav-link" href="<?= base_url('sales'); ?>">Main Page</a></li>
         </ul>
       </li>
 
@@ -26,19 +25,6 @@
         </ul>
       </li>
 
-      <li><a class="nav-link" href="<?= base_url('blank'); ?>"><i class="far fa-square"></i> <span>صفحه خالی</span></a></li>
-
-      <!-- <li class="menu-header">امکانات دیگر</li>
-      <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>کامپوننت ها</span></a>
-        <ul class="dropdown-menu">
-          <li><a class="nav-link" href="<?= base_url('components/gallery'); ?>">گالری</a></li>
-          <li><a class="nav-link" href="<?= base_url('components/upload'); ?>">مولتی آپلود</a></li>
-          <li><a class="nav-link" href="<?= base_url('components/tab'); ?>">تب</a></li>
-          <li><a class="nav-link" href="<?= base_url('components/table'); ?>">جدول</a></li>
-        </ul>
-      </li> -->
-
       <li class="dropdown">
         <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Helpdesk</span></a>
         <ul class="dropdown-menu">
@@ -49,23 +35,8 @@
         </ul>
       </li>
 
-      <!-- <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i> <span>خطاها</span></a>
-        <ul class="dropdown-menu">
-          <li><a class="nav-link" href="<?= base_url('errors/error_503'); ?>">503</a></li>
-          <li><a class="nav-link" href="<?= base_url('errors/error_403'); ?>">403</a></li>
-          <li><a class="nav-link" href="<?= base_url('errors/error_404'); ?>">404</a></li>
-          <li><a class="nav-link" href="<?= base_url('errors/error_500'); ?>">500</a></li>
-        </ul>
-      </li> -->
 
       <li><a class="nav-link" href="<?= base_url('credits'); ?>"><i class="fas fa-pencil-ruler"></i> <span>Question</span></a></li>
     </ul>
-
-    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" target="_blank" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Rocket
-      </a>
-    </div>
   </aside>
 </div>
